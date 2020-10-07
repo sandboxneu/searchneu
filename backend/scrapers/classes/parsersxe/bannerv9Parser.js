eac17ec260f148c95f48034fa5668c4b4669422c
Megan Li <meganlidou@gmail.com>
2020-10-06 21:13:39 -0400

    added college types and column to database schema

--- backend/scrapers/classes/parsersxe/bannerv9Parser.js
+++ backend/scrapers/classes/parsersxe/bannerv9Parser.js
@@ -20,10 +20,10 @@ const request = new Request('bannerv9Parser');
 class Bannerv9Parser {
   async main(termsUrl) {
     const termIds = (await this.getTermList(termsUrl)).map((t) => { return t.termId; });
-    const suffixes = ['10', '30', '40', '50', '60'];
-    const undergradIds = termIds.filter((t) => { return suffixes.includes(t.slice(-2)); }).slice(0, 6);
-    macros.log(`scraping terms: ${undergradIds}`);
-    return this.scrapeTerms(undergradIds);
+    // const suffixes = ['10', '30', '40', '50', '60'];
+    // const undergradIds = termIds.filter((t) => { return suffixes.includes(t.slice(-2)); }).slice(0, 6);
+    // macros.log(`scraping terms: ${undergradIds}`);
+    return this.scrapeTerms(['202058']);
   }
 
   /**
